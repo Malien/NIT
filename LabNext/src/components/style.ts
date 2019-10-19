c28fe4727f7fd775463a04d40e62d857b43c0480
Yaroslav <q.link0.p@gmail.com>
2019-10-20 02:43:49 +0300

    Updated assets and styles

--- LabNext/src/components/style.ts
+++ LabNext/src/components/style.ts
@@ -1,15 +1,21 @@
 import { createContext } from "react";
 
+type kind = "dark" | "light"
+
 export interface Theme {
+    kind: kind;
     headerColor: string;
     headerSubcolor: string;
     textColor: string;
-    subtextColor: string;
+    textSubcolor: string;
     alternateTextColor: string;
     alternateTextSubcolor: string;
     footerColor: string;
     backgroundColor: string;
     shadowColor: string;
+    alternativeColor: string;
+    alternativeSubcolor: string;
+    disabledColor: string;
 }
 
 export interface Look {
@@ -25,27 +31,35 @@ export interface Look {
 }
 
 export const Light: Theme = {
+    kind: "light",
     headerColor: "#3498db",
     headerSubcolor: "#5870D6",
     textColor: "#505050",
-    subtextColor: "#909090",
+    textSubcolor: "#5f5f5f",
     alternateTextColor: "#fff",
     alternateTextSubcolor: "#d6d6d6",
     footerColor: "#707070",
     backgroundColor: "#fff",
-    shadowColor: "##00000044"
+    shadowColor: "#00000044",
+    alternativeColor: "#aa00aa",
+    alternativeSubcolor: "purple",
+    disabledColor: "#606060"
 }
 
 export const Dark: Theme = {
+    kind: "dark",
     headerColor: "#4A6488",
     headerSubcolor: "#223C5B",
     footerColor: "#091C32",
     textColor: "#d8d8d8",
-    subtextColor: "#c2c2c2",
+    textSubcolor: "#949494",
     alternateTextColor: "#fff",
     alternateTextSubcolor: "#d6d6d6",
     backgroundColor: "#293045",
-    shadowColor: "#00000088"
+    shadowColor: "#00000088",
+    alternativeColor: "#aa00aa",
+    alternativeSubcolor: "purple",
+    disabledColor: "#383838"
 }
 
 export const Vanila: Look = {
